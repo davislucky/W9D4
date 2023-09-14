39bd12fdd7fbc66c1e06b72c045126aed3e170fc
davislucky <58579323+davislucky@users.noreply.github.com>
2023-09-14 15:40:39 -0700

    Finish phase 0 and finish up to Accept headers on Phase 1

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -2,9 +2,25 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
   options.headers = {
-    // Your code here
+    "X-CSRF-Token": csrfToken,
+    // "Content-type": "application/json",
+    "Accept": "application/json",
     ...options.headers
   };
 
   return await fetch(url, options);
 }
+
+const followUser = (id) => {
+  return customFetch(`/users/${id}/follow`, {
+    method: "POST",
+  });
+}
+
+const unfollowUser = (id) => {
+  return customFetch(`/users/${id}/follow`, {
+    method: "DELETE"
+  });
+}
+
+export { followUser, unfollowUser }
